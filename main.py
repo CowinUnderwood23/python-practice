bd7c56c4b024028210ccb6f27f6ef33a293eee21
Underwood Cowin <817281@stma.k12.mn.us>
2021-09-13 15:09:15 -0500

    yo

---
+++ main.py
@@ -0,0 +1,14 @@
+#print first food
+print ("popcorn")
+
+#print second food
+print ("brats")
+
+#print third food
+print ("cheeseburgers")
+
+#print fourth food
+print("ice cream")
+
+#print fifth food
+print ("chicken")
