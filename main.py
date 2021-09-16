ce9897866c8b45b1332328b7c12681a0d52f91bb
CowinUnderwood23 <817281@mystma.org>
2021-09-16 15:15:42 -0500

    mad lib

--- main.py
+++ main.py
@@ -73,3 +73,39 @@ def myNameIs(turtle):
 myNameIs("cowin")
 myNameIs("alex")
 
+def timeTen(num):
+
+    print(num * 10)
+
+timeTen(42)
+
+def plusFive(num):
+    
+    print(num + 5)
+
+plusFive(10)
+
+def dividedByTwo(num):
+
+    print(num / 2)
+
+dividedByTwo(12)
+
+print("-------------------")
+
+def addTwoNumbers(x, y, z):
+
+    print(x + y + z)
+
+addTwoNumbers(5, 10, 7)
+addTwoNumbers(-3, 8, 1)
+
+print("-------------------")
+
+def madLib(instrument, number, dayorweek, letter, majororminor, number1, number2, adj1, adj2, fraction, adj3, instrument2, adj4, noun, familymember, song, adjendingined, favartist):
+    
+    print("While I've stayed at home, I've practiced the " + instrument + " for " + number + " hours every " + dayorweek + ". My favorite key signature to play and practice in is " + letter + majororminor + ". It has " + number1 + " flats and " + number2 + " sharps. I like this key signature because it is " + adj1 + " and " + adj2 + ". I've also gotten awesome at counting time signatures. The time signature I love is " + fraction + " , because it is " + adj3 + " to count. One instrument I want to learn how to play while I'm at home is " + instrument2 + " , because it is " + adj4 + " and sounds like a " + noun + " . my " + familymember + " likes it when I play " + song + " , and always gives me a round of applause after my performances! when I return to school, my teacher will be " + adjendingined + " of how great I am at playing my instrument. My teacher might even think I sound like " + favartist + "!")
+
+madLib("piano", "24", "tuesday", "A ", "major", "six", "seven", "cool", "awesome", "1/5", "hard", "guitar", "interesting", "cat", "mom", "status", "astonished", "NAV")
+
+
