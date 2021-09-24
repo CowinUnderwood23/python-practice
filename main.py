d5d65edce1684e6279ff829c0ccf6b502c10e2c1
CowinUnderwood23 <817281@mystma.org>
2021-09-24 15:11:34 -0500

    if else

--- main.py
+++ main.py
@@ -1,58 +1,3 @@
-print("-top five movies")
-
-#define function
-def topFiveMovies():
-    #print first movie
-    print ("13 hours")
-     
-    #print second movie
-    print ("fast and furious")
-    
-    #print third movie
-    print ("hangover")
-
-    #print fourth movie
-    print ("bye bye man")
-
-    #print fifth movie
-    print ("step brothers")
-topFiveMovies()
-
-print("-top three sports")
-
-def topThreeSports():
-    print("hockey")
-    print("football")
-    print("basketball")
-topThreeSports()
-
-print("-family members")
-
-def familyMembers():
-    print("blake")
-    print("evan")
-    print("melissa")
-    print("mike")
-familyMembers()
-
-print("-top three foods")
-
-def topThreeFoods():
-    print("brats")
-    print("popcorn")
-    print("chicken")
-topThreeFoods()
-
-print("-top three candies")
-
-def topThreeCandies():
-    print("reeses")
-    print("M & M")
-    print("3 musketeers")
-topThreeCandies()
-
-print("--------------------")
-
 #integers dont need quotations
 #you can do math 
 print(100 + 65)
@@ -63,43 +8,6 @@ print("100" + "100")
 print("cat" + "dog")
 print("cat" * 10)
 
-print("--------------------")
-
-#giving our function a paramater and a variable
-
-def myNameIs(turtle):
-    print(turtle + " is my name")
-
-myNameIs("cowin")
-myNameIs("alex")
-
-def timeTen(num):
-
-    print(num * 10)
-
-timeTen(42)
-
-def plusFive(num):
-    
-    print(num + 5)
-
-plusFive(10)
-
-def dividedByTwo(num):
-
-    print(num / 2)
-
-dividedByTwo(12)
-
-print("-------------------")
-
-def addTwoNumbers(x, y, z):
-
-    print(x + y + z)
-
-addTwoNumbers(5, 10, 7)
-addTwoNumbers(-3, 8, 1)
-
 print("-------------------")
 
 def madLib(instrument, number, dayorweek, letter, majororminor, number1, number2, adj1, adj2, fraction, adj3, instrument2, adj4, noun, familymember, song, adjendingined, favartist):
@@ -108,4 +16,40 @@ def madLib(instrument, number, dayorweek, letter, majororminor, number1, number2
 
 madLib("piano", "24", "tuesday", "A ", "major", "six", "seven", "cool", "awesome", "1/5", "hard", "guitar", "interesting", "cat", "mom", "status", "astonished", "NAV")
 
+
+print("-------------------")
+
+def color():
+    return "red"
+
+print(color() + " is my favorite color")
+
+print("-------------------")
+
+def greaterThan10(x):
+    if  x > 10: 
+        return "x is greater than 10"
+
+    elif x == 10:
+        return "x equals 10"
+
+    else:
+        return "x is not greater than 10" 
+
+
+
+print(greaterThan10(10))
+
+
+def potato(x, y):
+    if x + y > 10:
+        return "potato"
+
+    elif x + y == 10:
+        return "tomato"
+
+    else:
+        return "alfredo"
+
+print(potato(5, 1))
 
